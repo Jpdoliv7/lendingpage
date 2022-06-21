b4a3da3ed287009cee43d67310d37ad009c65bfb
atouts20 <60098245+atouts20@users.noreply.github.com>
2022-06-21 18:20:05 +0100

    commit message

---
+++ docs/precache-manifest.0f14a45dc7a9a8af9a2f34ffed9a56da.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba35a221c20ada5671ee0256f18a0a9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15a010ec652dead58822",
+    "url": "/static/css/main.aeb9dcec.chunk.css"
+  },
+  {
+    "revision": "d9fa791521e15a9e1eb6",
+    "url": "/static/js/2.922d297b.chunk.js"
+  },
+  {
+    "revision": "f777c2cd8d43a44e423cc24a2b4ae71a",
+    "url": "/static/js/2.922d297b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a010ec652dead58822",
+    "url": "/static/js/main.c61d5de1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.c61d5de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "b64a560c149246ae0499d5c8b69ffff6",
+    "url": "/static/media/1.b64a560c.jpg"
+  },
+  {
+    "revision": "ff53d14652d9154a4abbca88aaca338c",
+    "url": "/static/media/2.ff53d146.jpg"
+  },
+  {
+    "revision": "1e09e2a9174793bcf5a829caaa701f57",
+    "url": "/static/media/3.1e09e2a9.jpg"
+  },
+  {
+    "revision": "98b8e8b269d078d764c73e81de4f2e00",
+    "url": "/static/media/4.98b8e8b2.jpg"
+  },
+  {
+    "revision": "e1e3e877a81f50a4832734ddc2a0d435",
+    "url": "/static/media/5.e1e3e877.jpg"
+  },
+  {
+    "revision": "501473f45f90d943cd3aeb5d553ce56c",
+    "url": "/static/media/6.501473f4.jpg"
+  },
+  {
+    "revision": "45bcb7d775d1dce4c70acb12ce907558",
+    "url": "/static/media/7.45bcb7d7.jpg"
+  },
+  {
+    "revision": "1e09e2a9174793bcf5a829caaa701f57",
+    "url": "/static/media/8.1e09e2a9.jpg"
+  },
+  {
+    "revision": "00aef28ea1c7d18022f14abe3f55167e",
+    "url": "/static/media/9.00aef28e.jpg"
+  },
+  {
+    "revision": "36dc7128285ef7e2025edbf4f650ebaa",
+    "url": "/static/media/head.36dc7128.jpg"
+  }
+]);
